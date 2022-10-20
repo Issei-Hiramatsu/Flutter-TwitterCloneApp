ff53cf54a67ca5e62af640879a942e3e93f84b7a
issei <issei.hiramatsu@gmail.com>
2022-10-20 16:01:39 +0900

    スクロールの際にステータスバーに重なってしまうバグを調整

--- lib/main.dart
+++ lib/main.dart
@@ -19,56 +19,59 @@ class TwitterBodyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white, //statusbarと色が被っているため
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            pinned: false,
-            floating: true,
-            elevation: 0,
-            leading: Builder(
-              builder: (BuildContext context) {
-                return SizedBox(
-                  width: 30.0,
-                  height: 30.0,
-                  child: TextButton(
-                    onPressed: () {},
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          fit: BoxFit.fitHeight,
-                          image: AssetImage('assets/images/face1.jpeg'),
+      body: SafeArea(
+        bottom: false,
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              pinned: false,
+              floating: true,
+              elevation: 0,
+              leading: Builder(
+                builder: (BuildContext context) {
+                  return SizedBox(
+                    width: 30.0,
+                    height: 30.0,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            fit: BoxFit.fitHeight,
+                            image: AssetImage('assets/images/face1.jpeg'),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                );
-              },
-            ),
-            title: Image.asset('assets/images/icon-480.png', height: 40),
-            centerTitle: true,
-            actions: [
-              Padding(
-                padding: const EdgeInsets.all(6),
-                child: IconButton(
-                    icon: const Icon(
-                      Icons.auto_awesome,
-                      size: 28,
-                      color: Colors.black,
-                    ),
-                    onPressed: () {}),
+                  );
+                },
               ),
-            ],
-            backgroundColor: Colors.white,
-            bottom: PreferredSize(
-              preferredSize: const Size.fromHeight(0.0),
-              child: Container(
-                color: Colors.grey.shade200,
-                height: 1.0,
+              title: Image.asset('assets/images/icon-480.png', height: 40),
+              centerTitle: true,
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.all(6),
+                  child: IconButton(
+                      icon: const Icon(
+                        Icons.auto_awesome,
+                        size: 28,
+                        color: Colors.black,
+                      ),
+                      onPressed: () {}),
+                ),
+              ],
+              backgroundColor: Colors.white,
+              bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(0.0),
+                child: Container(
+                  color: Colors.grey.shade200,
+                  height: 1.0,
+                ),
               ),
-            ),
-          ), // SliverAppBar
-        ],
+            ), // SliverAppBar
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
